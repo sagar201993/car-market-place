2d5b07b3d08780491dd9e85e7291e54058aced8f
sagar201993 <sagarvirk1@gmail.com>
2025-04-17 14:07:05 +0200

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ymqpkygmownybanldbpq.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "qfjtcrpjllrkmddrzmpj.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://roadsidecoder.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
